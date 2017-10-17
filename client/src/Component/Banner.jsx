7681d19181dfef2f0fdc3cfc167935d0ab0d7ec4
Tin Trung Dang <tindang1710@gmail.com>
2017-10-16 23:42:41 -0700

    start working on frontend

---
+++ client/src/Component/Banner.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className='banner'>
+        <h2>Rocket Watchlist</h2>
+      </div>
+    )
+  }
+}
+
+export default Banner;
